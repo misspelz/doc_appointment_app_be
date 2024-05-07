676d6364be8aeac4f64a9d5780a7ee225c5e99fc
misspelz <pelumiiadetoye@gmail.com>
2024-05-08 00:49:26 +0100

    added postgres

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host:  env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
-      port:  env('DATABASE_PORT', 5432),
-      database:  env('DATABASE_NAME', 'postgres'),
-      user:  env('DATABASE_USERNAME', 'postgres.irltvbkgzfzcnthhrqyr'),
-      password:  env('DATABASE_PASSWORD', '#Oluwapelumi2024'),
-      ssl:  env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "aws-0-eu-central-1.pooler.supabase.com"),
+      port: env("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "#Oluwapelumi2024"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
     useNullAsDefault: true,
   },
